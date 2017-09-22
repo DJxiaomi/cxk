f1fb0d6e8b2dd1d1e75ec345ac0eba26d2b36baf
Mr小米 <17666390+DJxiaomi@users.noreply.github.com>
2017-09-22 15:02:53 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+define('APP_DEBUG',True);
+
+define('APP_PATH','./App/');
+
+define('BUILD_CONTROLLER_LIST','Index,User,Admin,Order');
+
+require '/webdata/thinkphp_3.2.3/ThinkPHP/ThinkPHP.php';
